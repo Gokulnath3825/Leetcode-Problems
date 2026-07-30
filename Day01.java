62213f0d6d5aea2ade4aa9c9cafe8750744fe12c
Gokulnath <gokulj172@gmail.com>
2026-07-30 09:26:28 +0530

    Add files via upload

---
+++ Day01.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Day01 {
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int oddSum = n * n;
+        int evenSum = n * (n + 1);
+
+        System.out.println(gcd(oddSum, evenSum));
+
+        sc.close();
+    }
+}
