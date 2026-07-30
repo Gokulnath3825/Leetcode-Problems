c5e438ef5555b482484e45071e6669038526ab94
Gokulnath <gokulj172@gmail.com>
2026-07-30 15:03:56 +0530

    Add files via upload

---
+++ Day04.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Day04 {
+
+    public static int findGCD(int[] nums) {
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return gcd(min, max);
+    }
+
+    public static int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(findGCD(nums));
+    }
+}
