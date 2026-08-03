ed4087e8154fbe644a3f190e9ccf7f6907528b49
Gokulnath <gokulj172@gmail.com>
2026-08-03 21:31:09 +0530

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                sum += stoneValue[i + k];
+                dp[i] = Math.max(dp[i], sum - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
+    }
+}
+
+public class Day22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] stoneValue = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            stoneValue[i] = sc.nextInt();
+        }
+
+        Solution obj = new Solution();
+        System.out.println(obj.stoneGameIII(stoneValue));
+
+        sc.close();
+    }
+}
