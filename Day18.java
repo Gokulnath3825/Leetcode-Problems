0dcb03d018fc800bb575999c843be0a246f8c452
Gokulnath <gokulj172@gmail.com>
2026-08-04 09:20:37 +0530

    Refactor minimumPushes method in Day18.java
    Refactored minimumPushes method to remove unnecessary line breaks and improve readability.

--- Day18.java
+++ Day18.java
@@ -1,19 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
         int ans = 0;
-
         for (int i=0;i<n;i++) {
-            ans += (i / 8) + 1;
+            ans+=(i/8)+1;
         }
-
         return ans;
     }
 }
 
-public class Day18 {
+public class Day18{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
