ce04a18b7b9bc20dc9003dc950a81d9623541bdb
Gokulnath <gokulj172@gmail.com>
2026-08-08 19:52:53 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -2,32 +2,25 @@ import java.util.*;
 
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-
         int n = nums.length;
-
         if (n < 3) {
             return n;
         }
-
         int bits = 0;
         int x = n;
-
         while (x > 0) {
             bits++;
             x >>= 1;
         }
-
         return 1 << bits;
     }
 }
-
 public class Day11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         // Input size
         int n = sc.nextInt();
-
         // Input array
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
