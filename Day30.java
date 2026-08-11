4245bea12f346e122fb0c336ca50078da42584ad
Gokulnath <gokulj172@gmail.com>
2026-08-11 10:16:25 +0530

    Add files via upload

---
+++ Day30.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Day30 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int sum = nums[0];
+
+        // Find sequential prefix sum
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+
+        // Store all elements
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        // Find smallest missing integer
+        while (set.contains(sum)) {
+            sum++;
+        }
+
+        System.out.println(sum);
+    }
+}
