0a8f0403c65215857c7ec43bd97ae628e78d4c63
Gokulnath <gokulj172@gmail.com>
2026-08-16 23:42:31 +0530

    Clean up whitespace in Day29.java
    Refactor code by removing unnecessary whitespace.

--- Day29.java
+++ Day29.java
@@ -2,24 +2,17 @@ import java.util.*;
 
 public class Day29 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         boolean[] dp = new boolean[n + 1];
-
         for (int i = 1; i <= n; i++) {
-
             for (int j = 1; j * j <= i; j++) {
-
                 if (!dp[i - j * j]) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-
         System.out.println(dp[n]);
     }
 }
