5935f8a6cce03e94f030bafd30cd110701f7fbd4
Gokulnath <gokulj172@gmail.com>
2026-08-16 23:40:35 +0530

    Update Day33.java

--- Day33.java
+++ Day33.java
@@ -2,9 +2,13 @@ import java.util.*;
 
 public class Day33 {
 
+    
     public static int maximumLengthSubstring(String s) 
+    
         int[] cnt = new int[26];
+    
         int left = 0;
+    
         int ans = 0;
         for (int right = 0; right < s.length(); right++) {
             cnt[s.charAt(right) - 'a']++;
@@ -15,10 +19,12 @@ public class Day33 {
             ans = Math.max(ans, right - left + 1);
         }
         return ans;
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+        
         System.out.println(maximumLengthSubstring(s));
 
         sc.close();
