42d6d5207971631bb5de655f5a8461de328ca197
Gokulnath <gokulj172@gmail.com>
2026-08-20 13:32:17 +0530

    Refactor Day01.java for cleaner formatting

--- Day01.java
+++ Day01.java
@@ -1,7 +1,5 @@
 import java.util.*;
-
 public class Day01 {
-
     static int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
@@ -10,17 +8,12 @@ public class Day01 {
         }
         return a;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         int oddSum = n * n;
         int evenSum = n * (n + 1);
-
         System.out.println(gcd(oddSum, evenSum));
-
         sc.close();
     }
 }
