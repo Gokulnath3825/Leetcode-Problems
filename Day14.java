477352d90f7c186d890f0ae5bff099d4559bc44b
Gokulnath <gokulj172@gmail.com>
2026-08-21 09:41:06 +0530

    Clean up Day14.java by removing comments and whitespace
    Removed unnecessary comments and whitespace.

--- Day14.java
+++ Day14.java
@@ -1,33 +1,23 @@
 import java.util.*;
-
 class Solution {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-
         int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
         int option2 = nums[0] * nums[1] * nums[n - 1];
-
         return Math.max(option1, option2);
     }
 }
-
 public class Day14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Read array size
         int n = sc.nextInt();
-
-        // Read array elements
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-
         Solution obj = new Solution();
         System.out.println(obj.maximumProduct(nums));
-
         sc.close();
     }
 }
