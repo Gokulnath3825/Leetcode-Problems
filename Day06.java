3e90648bb10d3b03bd4db3e40b3c30a706628b93
Gokulnath <gokulj172@gmail.com>
2026-08-26 22:51:22 +0530

    Update Day06.java

--- Day06.java
+++ Day06.java
@@ -5,17 +5,27 @@ public class Day06 {
     public static List<List<Integer>> shiftGrid(int[][] grid, int k) {
 
         int m = grid.length;
+
         int n = grid[0].length;
+
         int total = m * n;
+
         k %= total;
+
         List<List<Integer>> result = new ArrayList<>();
+
         for (int i = 0; i < m; i++) {
+
             result.add(new ArrayList<>());
+
             for (int j = 0; j < n; j++) {
+
                 result.get(i).add(0);
+
             }
         }
         for (int i = 0; i < m; i++) {
+
             for (int j = 0; j < n; j++) {
                 int oldIndex = i * n + j;
                 int newIndex = (oldIndex + k) % total;
