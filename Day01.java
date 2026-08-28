b2b71855bb37b285565b4f31e6de86fe71514a52
Gokulnath <gokulj172@gmail.com>
2026-08-28 22:25:14 +0530

    Update Day01.java

--- Day01.java
+++ Day01.java
@@ -6,8 +6,11 @@ public class Day01 {
             b = a % b;
             a = temp;
         }
+        
         return a;
+        
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
