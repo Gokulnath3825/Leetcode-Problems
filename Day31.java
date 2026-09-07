ba7ec99556f14063b5a9fe6c2e08a942be96eaf2
Gokulnath <gokulj172@gmail.com>
2026-09-07 22:24:03 +0530

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -4,27 +4,22 @@ public class Day31 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int k = sc.nextInt();
-
         int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             nums[i] = sc.nextInt();
         }
-
         HashMap<Integer, Integer> map = new HashMap<>();
-
         int left = 0;
         int max = 0;
 
-        for (int right = 0; right < n; right++) {
+        for(int right = 0; right < n; right++){
 
             map.put(nums[right],
                 map.getOrDefault(nums[right], 0) + 1);
 
-            while (map.get(nums[right]) > k) {
+            while(map.get(nums[right]) > k){
                 map.put(nums[left],
                     map.get(nums[left]) - 1);
                 left++;
