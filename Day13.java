39bd6a344306998ee5351835d0864f3decc923a5
Gokulnath <gokulj172@gmail.com>
2026-09-14 19:33:24 +0530

    Update Day13.java

--- Day13.java
+++ Day13.java
@@ -1,10 +1,8 @@
 import java.util.*;
-
 class Solution {
     public int maxProduct(int n) {
         int first = 0;
         int second = 0;
-
         while (n > 0) {
             int digit = n % 10;
 
@@ -14,24 +12,17 @@ class Solution {
             } else if (digit > second) {
                 second = digit;
             }
-
             n /= 10;
         }
-
         return first * second;
     }
 }
 
-public class Day13 {
+public class Day13{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input number
         int n = sc.nextInt();
-
-        Solution obj = new Solution();
-        System.out.println(obj.maxProduct(n));
-
+        Solution obj = new Solution();      System.out.println(obj.maxProduct(n));
         sc.close();
     }
 }
